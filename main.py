34514b205845a5ef23b9e4058c01f9c06f67c3f5
alejo <alejolaverde0927@gmail.com>
2026-01-24 12:18:13 -0600

    Fix the paths in main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import json
 
 
 r_vals = {}
-with open("my_resistor_db.json", "r") as f:
+with open("combinations/my_resistor_db.json", "r") as f:
     r_vals = json.load(f)
     r_vals = {int(key): val for key, val in r_vals.items()}
 
 
 c_vals = {}
-with open("my_capacitor_db_3combos.json", "r") as f:
+with open("combinations/my_capacitor_db_3combos.json", "r") as f:
     c_vals = json.load(f)
 
     # Convert all from picofarads to float farads to do math with
