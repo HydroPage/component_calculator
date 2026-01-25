019eef1750ca11e1605f6ed0d99a81a8a2d3e483
alejo <alejolaverde0927@gmail.com>
2026-01-25 14:27:20 -0600

    Reconfigure main.py to solve equations after some weird git stuff lol

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from math import pi
 import json
+from component_formatting import format_resistor, format_capacitor
 
 
 r_vals = {}
-with open("combinations/my_resistor_db.json", "r") as f:
+with open("combinations/my_resistor_db_singles.json", "r") as f:
     r_vals = json.load(f)
     r_vals = {int(key): val for key, val in r_vals.items()}
 
 
 c_vals = {}
-with open("combinations/my_capacitor_db_3combos.json", "r") as f:
+with open("combinations/my_capacitor_db_2combos.json", "r") as f:
     c_vals = json.load(f)
 
     # Convert all from picofarads to float farads to do math with
@@ -17,7 +18,34 @@ with open("combinations/my_capacitor_db_3combos.json", "r") as f:
 
 
 def main():
-    print("Here we go")
+    min_err_square = 999999999
+    combo = {"C2": 0, "C3": 0, "R3": 0}
+    for r3, r3_combo in r_vals.items():
+        for c2, c2_combo in c_vals.items():
+            for c3, c3_combo in c_vals.items():
+                exp1_desired = 0.6
+                exp2_desired = 2*pi*4000
+                exp1 = c2 / (c2 + c3)
+                exp2 = 1/(r3*(c2+c3))
+
+                err1 = exp1 - exp1_desired
+                err2 = exp2 - exp2_desired
+
+                err1_percent = 100*(err1 / exp1_desired)
+                err2_percent = 100*(err2 / exp2_desired)
+
+                err_square = err1_percent**2 + err2_percent**2
+                if err_square < min_err_square:
+                    min_err_square = err_square
+                    combo["C2"] = c2
+                    combo["C3"] = c3
+                    combo["R3"] = r3
+
+                    print("-" * 50)
+                    print(f"err1: {err1_percent:<8.4f} % | err2: {err2_percent:<8.4f}%")
+                    print(f"C2 = {format_capacitor(c2)}: {c2_combo}")
+                    print(f"C3 = {format_capacitor(c3)}: {c3_combo}")
+                    print(f"R3 = {format_resistor(r3)}: {r3_combo}")
 
 
 if __name__ == "__main__":
