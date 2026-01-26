4c0955ae61a0481e2fe66d713e89f0d70da13f6e
alejo <alejolaverde0927@gmail.com>
2026-01-26 11:56:53 -0600

    Add elapsed time reporting to each new solution

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from math import pi
+from time import time, strftime, gmtime
 import json
 from component_formatting import format_resistor, format_capacitor
 
 
 r_vals = {}
-with open("combinations/my_resistor_db_singles.json", "r") as f:
+with open("combinations/my_resistor_db_3combos.json", "r") as f:
     r_vals = json.load(f)
     r_vals = {int(key): val for key, val in r_vals.items()}
 
@@ -20,6 +21,9 @@ with open("combinations/my_capacitor_db_2combos.json", "r") as f:
 def main():
     min_err_square = 999999999
     combo = {"C2": 0, "C3": 0, "R3": 0}
+
+    start_time_s = time()
+
     for r3, r3_combo in r_vals.items():
         for c2, c2_combo in c_vals.items():
             for c3, c3_combo in c_vals.items():
@@ -41,11 +45,14 @@ def main():
                     combo["C3"] = c3
                     combo["R3"] = r3
 
+                    elapsed_s_fmt = strftime('%H:%M:%S', gmtime(time() - start_time_s))
+
                     print("-" * 50)
                     print(f"err1: {err1_percent:<8.4f} % | err2: {err2_percent:<8.4f}%")
                     print(f"C2 = {format_capacitor(c2)}: {c2_combo}")
                     print(f"C3 = {format_capacitor(c3)}: {c3_combo}")
                     print(f"R3 = {format_resistor(r3)}: {r3_combo}")
+                    print(f"Elapsed time: {elapsed_s_fmt}")
 
 
 if __name__ == "__main__":
